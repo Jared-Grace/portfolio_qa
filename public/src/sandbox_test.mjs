23ae7561c24c8ba3729b7dec1add73ac3ab66dd6
Grace-Jared <christrosetolife@gmail.com>
2026-03-12 19:07:59 -0400

    function_auto_path ..\portfolio_qa\public\src\sandbox_test.mjs

--- public/src/sandbox_test.mjs
+++ public/src/sandbox_test.mjs
@@ -1,1 +1,8 @@
-export function sandbox_test() {}
+import { test, expect } from "@playwright/test";
+export function sandbox_test() {
+  async function lambda({ page }) {
+    await page.goto("https://example.com");
+    await expect(page).toHaveTitle(/Example/);
+  }
+  test("basic test", lambda);
+}
