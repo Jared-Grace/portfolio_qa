5b00af759eec493c9af8b615b737f85f34ca0bdd
Grace-Jared <christrosetolife@gmail.com>
2026-03-13 13:20:39 -0400

    function_auto_path ..\portfolio_qa\public\src\repo_functions_move_all.mjs

--- public/src/repo_functions_move_all.mjs
+++ public/src/repo_functions_move_all.mjs
@@ -1,6 +1,10 @@
+import { user_repo_set } from "../../../love/public/src/user_repo_set.mjs";
+import { arguments_assert } from "../../../love/public/src/arguments_assert.mjs";
 import { user_repo_get_functions_names } from "../../../portfolio_qa/public/src/user_repo_get_functions_names.mjs";
 import { repo_functions_move_acronym } from "../../../portfolio_qa/public/src/repo_functions_move_acronym.mjs";
-export async function repo_functions_move_all(to) {
+export async function repo_functions_move_all(from, to) {
+  arguments_assert(arguments, 2);
+  let repo_name2 = await user_repo_set(from);
   let filtered = await user_repo_get_functions_names();
   await repo_functions_move_acronym(to, filtered);
 }
