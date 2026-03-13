a1a6e25ba4f0253119a676161b6b1937c30fc4db
Grace-Jared <christrosetolife@gmail.com>
2026-03-13 12:54:48 -0400

    function_auto_path ..\portfolio_qa\public\src\sandbox_test.mjs

--- public/src/sandbox_test.mjs
+++ public/src/sandbox_test.mjs
@@ -1,8 +1,7 @@
-import { playwright_by_attribute_typed } from "../../../portfolio_qa/public/src/playwright_by_attribute_typed.mjs";
+import { playwright_by_attribute } from "../../../portfolio_qa/public/src/playwright_by_attribute.mjs";
 import { sleep } from "../../../love/public/src/sleep.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { chromium } from "playwright";
-const typed = "standard_user";
 export async function sandbox_test() {
   const browser = await chromium.launch({
     headless: false,
@@ -13,7 +12,8 @@ export async function sandbox_test() {
   console.log(title);
   const name = "data-test";
   const value = "username";
-  await playwright_by_attribute_typed(page, name, value);
+  const typed = "standard_user";
+  await playwright_by_attribute(page, name, value).type(typed);
   await page.locator('[data-test="password"]').type("secret_sauce");
   await page.locator('[data-test="login-button"]').click();
   await sleep(10000);
