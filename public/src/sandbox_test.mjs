55b1381583f0e4f95e5fef7e31ba02fa57926d07
Grace-Jared <christrosetolife@gmail.com>
2026-03-13 12:48:44 -0400

    function_auto_path ..\portfolio_qa\public\src\sandbox_test.mjs

--- public/src/sandbox_test.mjs
+++ public/src/sandbox_test.mjs
@@ -6,6 +6,8 @@ export async function sandbox_test() {
   await page.goto("https://www.saucedemo.com/");
   const title = await page.title();
   console.log(title);
-  page.locator('[data-test="username"]');
+  await page.locator('[data-test="username"]').fill("standard_user");
+  await page.locator('[data-test="password"]').fill("secret_sauce");
+  await page.locator('[data-test="login-button"]').click();
   await browser.close();
 }
