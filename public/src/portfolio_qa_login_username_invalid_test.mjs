3a8da9c8fb516637f97553292ebd66b28aa96030
Grace-Jared <christrosetolife@gmail.com>
2026-03-14 19:30:26 -0400

    function_auto_path ..\portfolio_qa\public\src\portfolio_qa_login_username_invalid_test.mjs

--- public/src/portfolio_qa_login_username_invalid_test.mjs
+++ public/src/portfolio_qa_login_username_invalid_test.mjs
@@ -1,12 +1,10 @@
 import { portfolio_qa_login_click } from "../../../portfolio_qa/public/src/portfolio_qa_login_click.mjs";
 import { portfolio_qa_by_attribute_type } from "../../../portfolio_qa/public/src/portfolio_qa_by_attribute_type.mjs";
-import { portfolio_qa_exists_assert } from "../../../portfolio_qa/public/src/portfolio_qa_exists_assert.mjs";
 import { portfolio_qa_error_username_password_invalid } from "../../../portfolio_qa/public/src/portfolio_qa_error_username_password_invalid.mjs";
 import { portfolio_qa_test_generic } from "../../../portfolio_qa/public/src/portfolio_qa_test_generic.mjs";
 export async function portfolio_qa_login_username_invalid_test() {
   async function lambda(page) {
     const invalid = "invalid";
-    await portfolio_qa_exists_assert(page, "login-container");
     await portfolio_qa_by_attribute_type(page, "username", "standard_user");
     await portfolio_qa_by_attribute_type(page, "password", invalid);
     await portfolio_qa_login_click(page);
