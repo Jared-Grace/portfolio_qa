9d0a6a2964befef8e1585b75419b7a688bd22b9e
Grace-Jared <christrosetolife@gmail.com>
2026-03-14 18:47:11 -0400

    function_auto_path ..\portfolio_qa\public\src\portfolio_qa_login_test.mjs

--- public/src/portfolio_qa_login_test.mjs
+++ public/src/portfolio_qa_login_test.mjs
@@ -1,3 +1,4 @@
+import { portfolio_qa_container_exists_assert } from "../../../portfolio_qa/public/src/portfolio_qa_container_exists_assert.mjs";
 import { playwright_by_attribute_exists_assert } from "../../../portfolio_qa/public/src/playwright_by_attribute_exists_assert.mjs";
 import { sleep_long } from "../../../portfolio_qa/public/src/sleep_long.mjs";
 import { portfolio_qa_username_valid } from "../../../portfolio_qa/public/src/portfolio_qa_username_valid.mjs";
@@ -8,11 +9,7 @@ export async function portfolio_qa_login_test() {
   await portfolio_qa_test_generic(lambda2);
   async function lambda2(page) {
     let value = "login";
-    await playwright_by_attribute_exists_assert(
-      page,
-      "data-test",
-      value + "-container",
-    );
+    await portfolio_qa_container_exists_assert(page, value);
     await portfolio_qa_username_valid(page);
     await playwright_by_attribute_type(
       page,
