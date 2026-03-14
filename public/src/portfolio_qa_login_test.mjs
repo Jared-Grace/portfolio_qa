47935ef013cf4254cb7f4f1bf8022afe85d58213
Grace-Jared <christrosetolife@gmail.com>
2026-03-14 19:07:58 -0400

    function_auto_path ..\portfolio_qa\public\src\portfolio_qa_login_test.mjs

--- public/src/portfolio_qa_login_test.mjs
+++ public/src/portfolio_qa_login_test.mjs
@@ -1,8 +1,8 @@
+import { portfolio_qa_by_attribute_type } from "../../../portfolio_qa/public/src/portfolio_qa_by_attribute_type.mjs";
 import { portfolio_qa_exists_not_assert } from "../../../portfolio_qa/public/src/portfolio_qa_exists_not_assert.mjs";
 import { portfolio_qa_exists_assert } from "../../../portfolio_qa/public/src/portfolio_qa_exists_assert.mjs";
 import { portfolio_qa_username_valid } from "../../../portfolio_qa/public/src/portfolio_qa_username_valid.mjs";
 import { portfolio_qa_login_click } from "../../../portfolio_qa/public/src/portfolio_qa_login_click.mjs";
-import { playwright_by_attribute_type } from "../../../love/public/src/playwright_by_attribute_type.mjs";
 import { portfolio_qa_test_generic } from "../../../portfolio_qa/public/src/portfolio_qa_test_generic.mjs";
 export async function portfolio_qa_login_test() {
   await portfolio_qa_test_generic(lambda2);
@@ -10,7 +10,7 @@ export async function portfolio_qa_login_test() {
     await portfolio_qa_exists_assert(page, "login-container");
     await portfolio_qa_username_valid(page);
     const password = "secret_sauce";
-    await playwright_by_attribute_type(page, "data-test", "password", password);
+    await portfolio_qa_by_attribute_type(page, "password", password);
     await portfolio_qa_login_click(page);
     await portfolio_qa_exists_not_assert(page, "login-container");
     await portfolio_qa_exists_assert(page, "inventory-container");
