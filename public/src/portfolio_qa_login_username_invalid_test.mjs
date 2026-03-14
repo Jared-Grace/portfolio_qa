3ea0491db439f52181409912dbddf47e1f3bc7c2
Grace-Jared <christrosetolife@gmail.com>
2026-03-14 19:28:57 -0400

    qa login_username_invalid

--- public/src/portfolio_qa_login_username_invalid_test.mjs
+++ public/src/portfolio_qa_login_username_invalid_test.mjs
@@ -1,1 +1,5 @@
-export function portfolio_qa_login_username_invalid_test() {}
+import { portfolio_qa_test_generic } from "../../../portfolio_qa/public/src/portfolio_qa_test_generic.mjs";
+export async function portfolio_qa_login_username_invalid_test() {
+  async function lambda(page) {}
+  await portfolio_qa_test_generic(lambda);
+}
