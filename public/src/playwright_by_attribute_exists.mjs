b6f946c630febe0680249c68334754522c9de2e1
Grace-Jared <christrosetolife@gmail.com>
2026-03-14 18:40:44 -0400

    function_auto_path ..\portfolio_qa\public\src\playwright_by_attribute_exists.mjs

--- public/src/playwright_by_attribute_exists.mjs
+++ public/src/playwright_by_attribute_exists.mjs
@@ -1,7 +1,7 @@
 import { greater_than_or_equal } from "../../../love/public/src/greater_than_or_equal.mjs";
 import { playwright_by_attribute_count } from "../../../portfolio_qa/public/src/playwright_by_attribute_count.mjs";
-export function playwright_by_attribute_exists(page, name, value) {
-  let c = playwright_by_attribute_count(page, name, value);
+export async function playwright_by_attribute_exists(page, name, value) {
+  let c = await playwright_by_attribute_count(page, name, value);
   let e = greater_than_or_equal(c, 1);
   return e;
 }
