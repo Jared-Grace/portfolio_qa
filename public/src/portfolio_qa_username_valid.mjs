203e1a7f1810c1ce535abb76b4a84d4784de8890
Grace-Jared <christrosetolife@gmail.com>
2026-03-14 19:30:42 -0400

    function_auto_path ..\portfolio_qa\public\src\portfolio_qa_username_valid.mjs

--- public/src/portfolio_qa_username_valid.mjs
+++ public/src/portfolio_qa_username_valid.mjs
@@ -1,4 +1,5 @@
 import { portfolio_qa_by_attribute_type } from "../../../portfolio_qa/public/src/portfolio_qa_by_attribute_type.mjs";
 export async function portfolio_qa_username_valid(page) {
-  await portfolio_qa_by_attribute_type(page, "username", "standard_user");
+  const value = "standard_user";
+  await portfolio_qa_by_attribute_type(page, "username", value);
 }
