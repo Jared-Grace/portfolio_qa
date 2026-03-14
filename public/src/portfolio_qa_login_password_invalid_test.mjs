51970800457e705b0fba1fe84a96029f2f7b6d90
Grace-Jared <christrosetolife@gmail.com>
2026-03-14 19:21:27 -0400

    function_auto_path ..\portfolio_qa\public\src\portfolio_qa_login_password_invalid_test.mjs

--- public/src/portfolio_qa_login_password_invalid_test.mjs
+++ public/src/portfolio_qa_login_password_invalid_test.mjs
@@ -1,4 +1,4 @@
-import { log } from "../../../love/public/src/log.mjs";
+import { portfolio_qa_error_message_required } from "../../../portfolio_qa/public/src/portfolio_qa_error_message_required.mjs";
 import { portfolio_qa_error_text } from "../../../portfolio_qa/public/src/portfolio_qa_error_text.mjs";
 import { portfolio_qa_username_valid_password_login } from "../../../portfolio_qa/public/src/portfolio_qa_username_valid_password_login.mjs";
 import { portfolio_qa_test_generic } from "../../../portfolio_qa/public/src/portfolio_qa_test_generic.mjs";
@@ -7,9 +7,7 @@ export async function portfolio_qa_login_password_invalid_test() {
     const invalid = "invalid";
     await portfolio_qa_username_valid_password_login(page, invalid);
     let actual = await portfolio_qa_error_text(page);
-    log(portfolio_qa_login_password_invalid_test.name, {
-      actual,
-    });
+    await portfolio_qa_error_message_required(page, "Password");
   }
   await portfolio_qa_test_generic(lambda);
 }
