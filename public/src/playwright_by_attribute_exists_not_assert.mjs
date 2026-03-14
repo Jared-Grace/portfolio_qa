a628b0212b10ee9dcbf07b70a1519a062854a971
Grace-Jared <christrosetolife@gmail.com>
2026-03-14 18:54:12 -0400

    function_auto_path ..\portfolio_qa\public\src\playwright_by_attribute_exists_not_assert.mjs

--- public/src/playwright_by_attribute_exists_not_assert.mjs
+++ public/src/playwright_by_attribute_exists_not_assert.mjs
@@ -1,11 +1,13 @@
+import { playwright_by_attribute_exists_assert_assertion } from "../../../portfolio_qa/public/src/playwright_by_attribute_exists_assert_assertion.mjs";
+import { playwright_by_attribute_exists } from "../../../portfolio_qa/public/src/playwright_by_attribute_exists.mjs";
 import { not } from "../../../love/public/src/not.mjs";
-import { playwright_by_attribute_exists_assert } from "../../../portfolio_qa/public/src/playwright_by_attribute_exists_assert.mjs";
 export async function playwright_by_attribute_exists_not_assert(
   page,
   name,
   value,
 ) {
-  let r = await playwright_by_attribute_exists_assert(page, name, value);
+  let e = await playwright_by_attribute_exists(page, name, value);
+  playwright_by_attribute_exists_assert_assertion(e, name, value);
   let n = not(r);
   return n;
 }
