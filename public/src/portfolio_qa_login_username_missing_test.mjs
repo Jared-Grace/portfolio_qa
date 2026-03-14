04c184570bbbb5cb477ed89a3e79468c16bfe9c7
Grace-Jared <christrosetolife@gmail.com>
2026-03-14 18:19:47 -0400

    function_auto_path ..\portfolio_qa\public\src\portfolio_qa_login_username_missing_test.mjs

--- public/src/portfolio_qa_login_username_missing_test.mjs
+++ public/src/portfolio_qa_login_username_missing_test.mjs
@@ -1,6 +1,5 @@
 import { equal_assert } from "../../../love/public/src/equal_assert.mjs";
 import { playwright_by_attribute_text } from "../../../portfolio_qa/public/src/playwright_by_attribute_text.mjs";
-import { log } from "../../../love/public/src/log.mjs";
 import { portfolio_qa_login_click } from "../../../portfolio_qa/public/src/portfolio_qa_login_click.mjs";
 import { portfolio_qa_test_generic } from "../../../portfolio_qa/public/src/portfolio_qa_test_generic.mjs";
 export async function portfolio_qa_login_username_missing_test() {
@@ -10,9 +9,6 @@ export async function portfolio_qa_login_username_missing_test() {
     const value = "error";
     let actual = await playwright_by_attribute_text(page, name, value);
     equal_assert(actual, "Epic sadface: Username is required");
-    log(portfolio_qa_login_username_missing_test.name, {
-      t,
-    });
   }
   await portfolio_qa_test_generic(lambda);
 }
