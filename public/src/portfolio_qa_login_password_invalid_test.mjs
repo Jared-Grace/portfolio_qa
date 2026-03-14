6c5e9cec57cd89e005069589676fcc50c1647303
Grace-Jared <christrosetolife@gmail.com>
2026-03-14 19:22:06 -0400

    function_auto_path ..\portfolio_qa\public\src\portfolio_qa_login_password_invalid_test.mjs

--- public/src/portfolio_qa_login_password_invalid_test.mjs
+++ public/src/portfolio_qa_login_password_invalid_test.mjs
@@ -1,12 +1,10 @@
 import { portfolio_qa_error_message_assert } from "../../../portfolio_qa/public/src/portfolio_qa_error_message_assert.mjs";
-import { portfolio_qa_error_text } from "../../../portfolio_qa/public/src/portfolio_qa_error_text.mjs";
 import { portfolio_qa_username_valid_password_login } from "../../../portfolio_qa/public/src/portfolio_qa_username_valid_password_login.mjs";
 import { portfolio_qa_test_generic } from "../../../portfolio_qa/public/src/portfolio_qa_test_generic.mjs";
 export async function portfolio_qa_login_password_invalid_test() {
   async function lambda(page) {
     const invalid = "invalid";
     await portfolio_qa_username_valid_password_login(page, invalid);
-    let actual = await portfolio_qa_error_text(page);
     await portfolio_qa_error_message_assert(
       page,
       "Username and password do not match any user in this service",
