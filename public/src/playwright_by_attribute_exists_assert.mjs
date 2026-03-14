55e0d70dd54ac91434278d9f69ed8f26c994b29f
Grace-Jared <christrosetolife@gmail.com>
2026-03-14 18:40:48 -0400

    function_auto_path ..\portfolio_qa\public\src\playwright_by_attribute_exists_assert.mjs

--- public/src/playwright_by_attribute_exists_assert.mjs
+++ public/src/playwright_by_attribute_exists_assert.mjs
@@ -1,7 +1,7 @@
 import { assert_json_get } from "../../../love/public/src/assert_json_get.mjs";
 import { playwright_by_attribute_exists } from "../../../portfolio_qa/public/src/playwright_by_attribute_exists.mjs";
-export function playwright_by_attribute_exists_assert(page, name, value) {
-  let e = playwright_by_attribute_exists(page, name, value);
+export async function playwright_by_attribute_exists_assert(page, name, value) {
+  let e = await playwright_by_attribute_exists(page, name, value);
   function lambda3() {
     let r = {
       exists: e,
