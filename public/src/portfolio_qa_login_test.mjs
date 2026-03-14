ba6bf6caa4d055df1fbde58c183c2c2f76d0a0b5
Grace-Jared <christrosetolife@gmail.com>
2026-03-14 18:36:35 -0400

    function_auto_path ..\portfolio_qa\public\src\portfolio_qa_login_test.mjs

--- public/src/portfolio_qa_login_test.mjs
+++ public/src/portfolio_qa_login_test.mjs
@@ -1,3 +1,4 @@
+import { g_tutorials_each } from "../../../love/public/src/g_tutorials_each.mjs";
 import { playwright_by_attribute_count } from "../../../portfolio_qa/public/src/playwright_by_attribute_count.mjs";
 import { sleep_long } from "../../../portfolio_qa/public/src/sleep_long.mjs";
 import { portfolio_qa_username_valid } from "../../../portfolio_qa/public/src/portfolio_qa_username_valid.mjs";
@@ -9,7 +10,9 @@ export async function portfolio_qa_login_test() {
   async function lambda2(page) {
     let name = "data-test";
     let value = "login-container";
-    let c = playwright_by_attribute_count(page, name, value);gte
+    let c = playwright_by_attribute_count(page, name, value);
+    function lambda(tutorial) {}
+    g_tutorials_each(lambda);
     await portfolio_qa_username_valid(page);
     await playwright_by_attribute_type(
       page,
