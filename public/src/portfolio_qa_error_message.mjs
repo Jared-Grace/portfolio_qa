a4dc2c19a1c74c1740a05264e670110cd552c7e5
Grace-Jared <christrosetolife@gmail.com>
2026-03-14 19:14:33 -0400

    function_auto_path ..\portfolio_qa\public\src\portfolio_qa_error_message.mjs

--- public/src/portfolio_qa_error_message.mjs
+++ public/src/portfolio_qa_error_message.mjs
@@ -1,7 +1,7 @@
 import { portfolio_qa_error_text } from "../../../portfolio_qa/public/src/portfolio_qa_error_text.mjs";
 import { equal_assert } from "../../../love/public/src/equal_assert.mjs";
 export async function portfolio_qa_error_message(page, name_field) {
-  const details = name_field + " is required";
+  const expected = name_field + " is required";
   let actual = await portfolio_qa_error_text(page);
-  equal_assert(actual, "Epic sadface: " + details);
+  equal_assert(actual, "Epic sadface: " + expected);
 }
