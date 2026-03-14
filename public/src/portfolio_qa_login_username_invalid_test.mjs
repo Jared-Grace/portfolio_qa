3307f999b6e4d85aa7af1283702bcefacdc7435c
Grace-Jared <christrosetolife@gmail.com>
2026-03-14 19:29:54 -0400

    function_auto_path ..\portfolio_qa\public\src\portfolio_qa_login_username_invalid_test.mjs

--- public/src/portfolio_qa_login_username_invalid_test.mjs
+++ public/src/portfolio_qa_login_username_invalid_test.mjs
@@ -1,10 +1,16 @@
+import { portfolio_qa_login_click } from "../../../portfolio_qa/public/src/portfolio_qa_login_click.mjs";
+import { portfolio_qa_by_attribute_type } from "../../../portfolio_qa/public/src/portfolio_qa_by_attribute_type.mjs";
+import { portfolio_qa_username_valid } from "../../../portfolio_qa/public/src/portfolio_qa_username_valid.mjs";
+import { portfolio_qa_exists_assert } from "../../../portfolio_qa/public/src/portfolio_qa_exists_assert.mjs";
 import { portfolio_qa_error_username_password_invalid } from "../../../portfolio_qa/public/src/portfolio_qa_error_username_password_invalid.mjs";
-import { portfolio_qa_username_valid_password_login } from "../../../portfolio_qa/public/src/portfolio_qa_username_valid_password_login.mjs";
 import { portfolio_qa_test_generic } from "../../../portfolio_qa/public/src/portfolio_qa_test_generic.mjs";
 export async function portfolio_qa_login_username_invalid_test() {
   async function lambda(page) {
     const invalid = "invalid";
-    await portfolio_qa_username_valid_password_login(page, invalid);
+    await portfolio_qa_exists_assert(page, "login-container");
+    await portfolio_qa_username_valid(page);
+    await portfolio_qa_by_attribute_type(page, "password", invalid);
+    await portfolio_qa_login_click(page);
     await portfolio_qa_error_username_password_invalid(page);
   }
   await portfolio_qa_test_generic(lambda);
