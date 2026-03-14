ff0a09b156d26d2f7d3794b4f3cc95fdc7c7dadf
Grace-Jared <christrosetolife@gmail.com>
2026-03-14 18:49:31 -0400

    cpi portfolio_qa_container_exists_assert assert not

---
+++ public/src/portfolio_qa_container_exists_not_assert.mjs
@@ -0,0 +1,8 @@
+import { playwright_by_attribute_exists_assert } from "../../../portfolio_qa/public/src/playwright_by_attribute_exists_assert.mjs";
+export async function portfolio_qa_container_exists_not_assert(page, value) {
+  await playwright_by_attribute_exists_assert(
+    page,
+    "data-test",
+    value + "-container",
+  );
+}
