d4e90751e6c368dbf45fa55991cbb27893f5384a
Grace-Jared <christrosetolife@gmail.com>
2026-03-14 18:48:22 -0400

    fws playwright_by_attribute_exists_assert not

---
+++ public/src/playwright_by_attribute_exists_assert_not.mjs
@@ -0,0 +1,8 @@
+import { playwright_by_attribute_exists_assert } from "../../../portfolio_qa/public/src/playwright_by_attribute_exists_assert.mjs";
+export async function playwright_by_attribute_exists_assert_not(
+  page,
+  name,
+  value,
+) {
+  return await playwright_by_attribute_exists_assert(page, name, value);
+}
