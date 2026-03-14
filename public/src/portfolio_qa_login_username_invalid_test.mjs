4fe1d1da31f1f50e1d4e0a82bfa01bc59f8994aa
Grace-Jared <christrosetolife@gmail.com>
2026-03-14 19:31:12 -0400

    function_auto_path ..\portfolio_qa\public\src\portfolio_qa_login_username_invalid_test.mjs

--- public/src/portfolio_qa_login_username_invalid_test.mjs
+++ public/src/portfolio_qa_login_username_invalid_test.mjs
@@ -6,7 +6,7 @@ import { portfolio_qa_test_generic } from "../../../portfolio_qa/public/src/port
 export async function portfolio_qa_login_username_invalid_test() {
   async function lambda(page) {
     const invalid = "invalid";
-    await portfolio_qa_username(page, value);
+    await portfolio_qa_username(page, "invalid");
     await portfolio_qa_by_attribute_type(page, "password", invalid);
     await portfolio_qa_login_click(page);
     await portfolio_qa_error_username_password_invalid(page);
