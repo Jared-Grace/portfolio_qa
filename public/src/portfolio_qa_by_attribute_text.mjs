d3a0762c512caaca0b8dfbf921a11bc9ed88b8e3
Grace-Jared <christrosetolife@gmail.com>
2026-03-14 19:12:24 -0400

    function_auto_path ..\portfolio_qa\public\src\portfolio_qa_by_attribute_text.mjs

--- public/src/portfolio_qa_by_attribute_text.mjs
+++ public/src/portfolio_qa_by_attribute_text.mjs
@@ -1,5 +1,5 @@
 import { playwright_by_attribute_text } from "../../../portfolio_qa/public/src/playwright_by_attribute_text.mjs";
-export async function portfolio_qa_by_attribute(page, value) {
+export async function portfolio_qa_by_attribute_text(page, value) {
   const name = "data-test";
   let actual = await playwright_by_attribute_text(page, name, value);
   return actual;
--- public/src/portfolio_qa_error_message.mjs
+++ public/src/portfolio_qa_error_message.mjs
@@ -1,7 +1,7 @@
-import { portfolio_qa_by_attribute } from "../../../portfolio_qa/public/src/portfolio_qa_by_attribute.mjs";
+import { portfolio_qa_by_attribute_text } from "../../../portfolio_qa/public/src/portfolio_qa_by_attribute_text.mjs";
 import { equal_assert } from "../../../love/public/src/equal_assert.mjs";
 export async function portfolio_qa_error_message(page, name_field) {
   const value = "error";
-  let actual = await portfolio_qa_by_attribute(page, value);
+  let actual = await portfolio_qa_by_attribute_text(page, value);
   equal_assert(actual, "Epic sadface: " + name_field + " is required");
 }
