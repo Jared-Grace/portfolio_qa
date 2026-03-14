6bed91bd55032e1d981c598356cdf0cf7d3ee208
Grace-Jared <christrosetolife@gmail.com>
2026-03-14 18:19:30 -0400

    function_auto_path ..\portfolio_qa\public\src\portfolio_qa_login_username_missing_test.mjs

--- public/src/portfolio_qa_login_username_missing_test.mjs
+++ public/src/portfolio_qa_login_username_missing_test.mjs
@@ -2,7 +2,6 @@ import { equal_assert } from "../../../love/public/src/equal_assert.mjs";
 import { playwright_by_attribute_text } from "../../../portfolio_qa/public/src/playwright_by_attribute_text.mjs";
 import { log } from "../../../love/public/src/log.mjs";
 import { portfolio_qa_login_click } from "../../../portfolio_qa/public/src/portfolio_qa_login_click.mjs";
-import { sleep } from "../../../love/public/src/sleep.mjs";
 import { portfolio_qa_test_generic } from "../../../portfolio_qa/public/src/portfolio_qa_test_generic.mjs";
 export async function portfolio_qa_login_username_missing_test() {
   async function lambda(page) {
