ecfd0c7810877034fba2b690f6fbf72e3bc2c50c
Grace-Jared <christrosetolife@gmail.com>
2026-03-14 18:11:13 -0400

    function_auto_path ..\portfolio_qa\public\src\portfolio_qa_login_username_missing_test.mjs

--- public/src/portfolio_qa_login_username_missing_test.mjs
+++ public/src/portfolio_qa_login_username_missing_test.mjs
@@ -1,5 +1,8 @@
+import { portfolio_qa_login_click } from "../../../portfolio_qa/public/src/portfolio_qa_login_click.mjs";
 import { portfolio_qa_test_generic } from "../../../portfolio_qa/public/src/portfolio_qa_test_generic.mjs";
 export async function portfolio_qa_login_username_missing_test() {
-  async function lambda(page) {}
+  async function lambda(page) {
+    await portfolio_qa_login_click(page);
+  }
   await portfolio_qa_test_generic(lambda);
 }
