8018f2206dc4428c0fd7642da8201564e7dbf4ae
Grace-Jared <christrosetolife@gmail.com>
2026-03-14 18:51:26 -0400

    function_auto_path ..\portfolio_qa\public\src\portfolio_qa_login_test.mjs

--- public/src/portfolio_qa_login_test.mjs
+++ public/src/portfolio_qa_login_test.mjs
@@ -8,7 +8,7 @@ import { portfolio_qa_test_generic } from "../../../portfolio_qa/public/src/port
 export async function portfolio_qa_login_test() {
   await portfolio_qa_test_generic(lambda2);
   async function lambda2(page) {
-    await portfolio_qa_container_exists_assert(page, "login");
+    await portfolio_qa_container_exists_assert(page, "login-container");
     await portfolio_qa_username_valid(page);
     await playwright_by_attribute_type(
       page,
@@ -17,8 +17,8 @@ export async function portfolio_qa_login_test() {
       "secret_sauce",
     );
     await portfolio_qa_login_click(page);
-    await portfolio_qa_container_exists_not_assert(page, "login");
-    await portfolio_qa_container_exists_assert(page, "inventory");
+    await portfolio_qa_container_exists_not_assert(page, "login-container");
+    await portfolio_qa_container_exists_assert(page, "inventory-container");
     await sleep_long();
   }
 }
