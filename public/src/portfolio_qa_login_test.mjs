dc944d2e124bb6606b364e73057860f5179e515c
Grace-Jared <christrosetolife@gmail.com>
2026-03-14 18:23:57 -0400

    function_auto_path ..\portfolio_qa\public\src\portfolio_qa_login_test.mjs

--- public/src/portfolio_qa_login_test.mjs
+++ public/src/portfolio_qa_login_test.mjs
@@ -1,15 +1,11 @@
+import { portfolio_qa_username_valid } from "../../../portfolio_qa/public/src/portfolio_qa_username_valid.mjs";
 import { portfolio_qa_login_click } from "../../../portfolio_qa/public/src/portfolio_qa_login_click.mjs";
 import { playwright_by_attribute_type } from "../../../love/public/src/playwright_by_attribute_type.mjs";
 import { portfolio_qa_test_generic } from "../../../portfolio_qa/public/src/portfolio_qa_test_generic.mjs";
 export async function portfolio_qa_login_test() {
   await portfolio_qa_test_generic(lambda2);
   async function lambda2(page) {
-    await playwright_by_attribute_type(
-      page,
-      "data-test",
-      "username",
-      "standard_user",
-    );
+    await portfolio_qa_username_valid(page);
     await playwright_by_attribute_type(
       page,
       "data-test",
---
+++ public/src/portfolio_qa_username_valid.mjs
@@ -0,0 +1,9 @@
+import { playwright_by_attribute_type } from "../../../love/public/src/playwright_by_attribute_type.mjs";
+export async function portfolio_qa_username_valid(page) {
+  await playwright_by_attribute_type(
+    page,
+    "data-test",
+    "username",
+    "standard_user",
+  );
+}
