edacfc30b31f8f0148846873e55ec3013ec9b29a
Grace-Jared <christrosetolife@gmail.com>
2026-03-14 19:07:02 -0400

    function_auto_path ..\portfolio_qa\public\src\portfolio_qa_by_attribute_type.mjs

--- public/src/portfolio_qa_by_attribute_type.mjs
+++ public/src/portfolio_qa_by_attribute_type.mjs
@@ -1,5 +1,5 @@
 import { playwright_by_attribute_type } from "../../../love/public/src/playwright_by_attribute_type.mjs";
-export async function portfolio_qa_by_attribute_type(page, name, value, typed) {
-  let r = await playwright_by_attribute_type(page, name, value, typed);
+export async function portfolio_qa_by_attribute_type(page, value, typed) {
+  let r = await playwright_by_attribute_type(page, "data-test", value, typed);
   return r;
 }
