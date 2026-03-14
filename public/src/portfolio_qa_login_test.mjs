1e6d53fe10c7dae89965e6361fe80a058fa8f38e
Grace-Jared <christrosetolife@gmail.com>
2026-03-14 18:49:48 -0400

    function_auto_path ..\portfolio_qa\public\src\portfolio_qa_login_test.mjs

--- public/src/portfolio_qa_login_test.mjs
+++ public/src/portfolio_qa_login_test.mjs
@@ -1,3 +1,4 @@
+import { portfolio_qa_container_exists_not_assert } from "../../../portfolio_qa/public/src/portfolio_qa_container_exists_not_assert.mjs";
 import { portfolio_qa_container_exists_assert } from "../../../portfolio_qa/public/src/portfolio_qa_container_exists_assert.mjs";
 import { sleep_long } from "../../../portfolio_qa/public/src/sleep_long.mjs";
 import { portfolio_qa_username_valid } from "../../../portfolio_qa/public/src/portfolio_qa_username_valid.mjs";
@@ -16,6 +17,7 @@ export async function portfolio_qa_login_test() {
       "secret_sauce",
     );
     await portfolio_qa_login_click(page);
+    await portfolio_qa_container_exists_not_assert(page2, value);
     await portfolio_qa_container_exists_assert(page, "inventory");
     await sleep_long();
   }
