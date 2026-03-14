c7f2918633248aa7f8fca1f110f188ed2118faf8
Grace-Jared <christrosetolife@gmail.com>
2026-03-14 18:23:03 -0400

    cr portfolio_qa_login_username_missing_test username password

---
+++ public/src/portfolio_qa_login_password_missing_test.mjs
@@ -0,0 +1,14 @@
+import { equal_assert } from "../../../love/public/src/equal_assert.mjs";
+import { playwright_by_attribute_text } from "../../../portfolio_qa/public/src/playwright_by_attribute_text.mjs";
+import { portfolio_qa_login_click } from "../../../portfolio_qa/public/src/portfolio_qa_login_click.mjs";
+import { portfolio_qa_test_generic } from "../../../portfolio_qa/public/src/portfolio_qa_test_generic.mjs";
+export async function portfolio_qa_login_password_missing_test() {
+  async function lambda(page) {
+    await portfolio_qa_login_click(page);
+    const name = "data-test";
+    const value = "error";
+    let actual = await playwright_by_attribute_text(page, name, value);
+    equal_assert(actual, "Epic sadface: Username is required");
+  }
+  await portfolio_qa_test_generic(lambda);
+}
