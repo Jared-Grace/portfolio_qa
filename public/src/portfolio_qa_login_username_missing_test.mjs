d9c7a59f824afd4662735deb12b9a622b879b163
Grace-Jared <christrosetolife@gmail.com>
2026-03-14 18:14:25 -0400

    function_auto_path ..\portfolio_qa\public\src\portfolio_qa_login_username_missing_test.mjs

--- public/src/portfolio_qa_login_username_missing_test.mjs
+++ public/src/portfolio_qa_login_username_missing_test.mjs
@@ -1,10 +1,11 @@
+import { portfolio_qa_login_click } from "../../../portfolio_qa/public/src/portfolio_qa_login_click.mjs";
 import { playwright_by_attribute } from "../../../love/public/src/playwright_by_attribute.mjs";
 import { sleep } from "../../../love/public/src/sleep.mjs";
 import { portfolio_qa_test_generic } from "../../../portfolio_qa/public/src/portfolio_qa_test_generic.mjs";
 export async function portfolio_qa_login_username_missing_test() {
   async function lambda(page) {
     await playwright_by_attribute(page, name, value).textContent();
-    await playwright_by_attribute(page, name, value).click();
+    await portfolio_qa_login_click(page);
     await sleep(99999);
   }
   await portfolio_qa_test_generic(lambda);
