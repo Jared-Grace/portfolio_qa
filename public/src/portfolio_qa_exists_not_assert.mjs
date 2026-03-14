a10ffe88b1e878f5bceede1ba51a1c5d4db9b5d0
Grace-Jared <christrosetolife@gmail.com>
2026-03-14 18:53:08 -0400

    function_auto_path ..\portfolio_qa\public\src\portfolio_qa_exists_not_assert.mjs

--- public/src/portfolio_qa_exists_not_assert.mjs
+++ public/src/portfolio_qa_exists_not_assert.mjs
@@ -1,8 +1,4 @@
 import { playwright_by_attribute_exists_not_assert } from "../../../portfolio_qa/public/src/playwright_by_attribute_exists_not_assert.mjs";
 export async function portfolio_qa_exists_not_assert(page, value) {
-  await playwright_by_attribute_exists_not_assert(
-    page,
-    "data-test",
-    value + "-container",
-  );
+  await playwright_by_attribute_exists_not_assert(page, "data-test", value);
 }
