1a93c67298a9b1b914b3340b6a705546af8e62e9
Grace-Jared <christrosetolife@gmail.com>
2026-03-15 14:33:17 -0400

    function_auto_path ..\portfolio_qa\public\src\portfolio_qa_login_locked_test.mjs

--- public/src/portfolio_qa_login_locked_test.mjs
+++ public/src/portfolio_qa_login_locked_test.mjs
@@ -1,3 +1,4 @@
+import { playwright_by_attribute } from "../../../love/public/src/playwright_by_attribute.mjs";
 import { portfolio_qa_username } from "../../../portfolio_qa/public/src/portfolio_qa_username.mjs";
 import { portfolio_qa_login_click } from "../../../portfolio_qa/public/src/portfolio_qa_login_click.mjs";
 import { portfolio_qa_password_type } from "../../../portfolio_qa/public/src/portfolio_qa_password_type.mjs";
@@ -7,6 +8,7 @@ export async function portfolio_qa_login_locked_test() {
   async function lambda(page) {
     const password = portfolio_qa_password_valid();
     const value = "locked_out_user";
+    let r = playwright_by_attribute(page2, name, value2);
     await portfolio_qa_username(page, value);
     await portfolio_qa_password_type(page, password);
     await portfolio_qa_login_click(page);
