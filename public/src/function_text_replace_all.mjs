fbf8662facac4f99a8903f02a0eb96e3b02b9a26
Grace-Jared <christrosetolife@gmail.com>
2026-03-15 15:19:50 -0400

    function_auto_path ..\portfolio_qa\public\src\function_text_replace_all.mjs

--- public/src/function_text_replace_all.mjs
+++ public/src/function_text_replace_all.mjs
@@ -2,7 +2,7 @@ import { function_run } from "../../../love/public/src/function_run.mjs";
 import { function_text_replace_all_lambda } from "../../../portfolio_qa/public/src/function_text_replace_all_lambda.mjs";
 import { functions_transform } from "../../../love/public/src/functions_transform.mjs";
 export async function function_text_replace_all(f_name, text) {
-  let result = await function_run(f_name2, args);
+  let result = await function_run(f_name, []);
   async function lambda2(ast) {
     function_text_replace_all_lambda(ast, f_name, text);
   }
