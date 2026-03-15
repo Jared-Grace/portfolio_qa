1147855b4af9654c9841a7558a3248fc73cd806f
Grace-Jared <christrosetolife@gmail.com>
2026-03-15 14:38:48 -0400

    function_auto_path ..\portfolio_qa\public\src\function_new_text_replace_all.mjs

--- public/src/function_new_text_replace_all.mjs
+++ public/src/function_new_text_replace_all.mjs
@@ -1,5 +1,5 @@
 import { function_new_transform } from "../../../love/public/src/function_new_transform.mjs";
-export async function function_new_text_replace_all() {
+export async function function_new_text_replace_all(text, f_name) {
   async function lambda(ast) {}
   let output = await function_new_transform(f_name, lambda);
 }
