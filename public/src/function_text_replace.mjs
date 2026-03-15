555fcab4090294bdd3aae4217f602fc2f42c7a6a
Grace-Jared <christrosetolife@gmail.com>
2026-03-15 15:23:34 -0400

    function_auto_path ..\portfolio_qa\public\src\function_text_replace.mjs

--- public/src/function_text_replace.mjs
+++ public/src/function_text_replace.mjs
@@ -1,6 +1,6 @@
 import { function_transform } from "../../../love/public/src/function_transform.mjs";
 import { function_text_replace_all_transform } from "../../../portfolio_qa/public/src/function_text_replace_all_transform.mjs";
-export async function function_text_replace(f_name_call) {
+export async function function_text_replace(f_name, f_name_call) {
   let t = await function_text_replace_all_transform(f_name_call);
-  let waited = await function_transform(t);
+  let waited = await function_transform(f_name, t);
 }
