a2b9526102c85f2412463535812c6561155d8b09
Grace-Jared <christrosetolife@gmail.com>
2026-03-15 15:07:04 -0400

    function_auto_path ..\portfolio_qa\public\src\function_new_text_replace_all.mjs

--- public/src/function_new_text_replace_all.mjs
+++ public/src/function_new_text_replace_all.mjs
@@ -15,10 +15,10 @@ export async function function_new_text_replace_all(text, f_name) {
   let output = await function_new_transform(f_name, lambda);
   async function lambda2(ast) {
     let nodes = js_list_type_nodes(ast, "Literal");
-    function lambda3(item) {
+    function lambda3(literal) {
       let value = js_literal_value_get(literal);
     }
-    each(list, lambda3);
+    each(nodes, lambda3);
   }
   let waited = await functions_transform(lambda2);
 }
