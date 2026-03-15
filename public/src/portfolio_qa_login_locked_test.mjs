72b9a686e3b1912b8d7c78e180b8a7e645ac7545
Grace-Jared <christrosetolife@gmail.com>
2026-03-15 14:31:31 -0400

    function_auto_path ..\portfolio_qa\public\src\portfolio_qa_login_locked_test.mjs

--- public/src/portfolio_qa_login_locked_test.mjs
+++ public/src/portfolio_qa_login_locked_test.mjs
@@ -5,7 +5,7 @@ import { portfolio_qa_password_valid } from "../../../portfolio_qa/public/src/po
 import { portfolio_qa_test_generic } from "../../../portfolio_qa/public/src/portfolio_qa_test_generic.mjs";
 export async function portfolio_qa_login_locked_test() {
   async function lambda(page) {
-    const password = portfolio_qa_password_valid();
+    const password = portfolio_qa_password_valid();$x
     await portfolio_qa_username_valid(page);
     await portfolio_qa_password_type(page, password);
     await portfolio_qa_login_click(page);
