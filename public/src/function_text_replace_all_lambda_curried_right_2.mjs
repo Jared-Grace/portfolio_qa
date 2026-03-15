6b395c1a311e00cd04cce4d5e4a5a882bb01aee4
Grace-Jared <christrosetolife@gmail.com>
2026-03-15 15:21:09 -0400

    function_auto_path ..\portfolio_qa\public\src\function_text_replace_all_lambda_curried_right_2.mjs

--- public/src/function_text_replace_all_lambda_curried_right_2.mjs
+++ public/src/function_text_replace_all_lambda_curried_right_2.mjs
@@ -1,6 +1,10 @@
 import { function_text_replace_all_lambda } from "../../../portfolio_qa/public/src/function_text_replace_all_lambda.mjs";
 export function function_text_replace_all_lambda_curried_right_2(f_name, text) {
-  return function function_text_replace_all_lambda_curried_right_2_result(ast) {
-    return function_text_replace_all_lambda(ast, f_name, text);
+  let r2 = function function_text_replace_all_lambda_curried_right_2_result(
+    ast,
+  ) {
+    let r = function_text_replace_all_lambda(ast, f_name, text);
+    return r;
   };
+  return r2;
 }
