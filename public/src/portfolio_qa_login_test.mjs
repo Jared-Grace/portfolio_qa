5141d77a4845386775375adf5b2f2fa0f0d9c839
Grace-Jared <christrosetolife@gmail.com>
2026-03-14 20:01:37 -0400

    function_auto_path ..\portfolio_qa\public\src\portfolio_qa_login_test.mjs

--- public/src/portfolio_qa_login_test.mjs
+++ public/src/portfolio_qa_login_test.mjs
@@ -1,3 +1,4 @@
+import { portfolio_qa_password_valid } from "../../../portfolio_qa/public/src/portfolio_qa_password_valid.mjs";
 import { portfolio_qa_username_valid_password_login } from "../../../portfolio_qa/public/src/portfolio_qa_username_valid_password_login.mjs";
 import { portfolio_qa_exists_not_assert } from "../../../portfolio_qa/public/src/portfolio_qa_exists_not_assert.mjs";
 import { portfolio_qa_exists_assert } from "../../../portfolio_qa/public/src/portfolio_qa_exists_assert.mjs";
@@ -5,7 +6,7 @@ import { portfolio_qa_test_generic } from "../../../portfolio_qa/public/src/port
 export async function portfolio_qa_login_test() {
   await portfolio_qa_test_generic(lambda2);
   async function lambda2(page) {
-    const password = "secret_sauce";
+    const password = portfolio_qa_password_valid();
     await portfolio_qa_username_valid_password_login(page, password);
     await portfolio_qa_exists_not_assert(page, "login-container");
     await portfolio_qa_exists_assert(page, "inventory-container");
---
+++ public/src/portfolio_qa_password_valid.mjs
@@ -0,0 +1,4 @@
+export function portfolio_qa_password_valid() {
+  let r = "secret_sauce";
+  return r;
+}
