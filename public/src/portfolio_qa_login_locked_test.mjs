2dd6c0e209e23ce46b284905a5da909bf1d0f2c1
Grace-Jared <christrosetolife@gmail.com>
2026-03-15 14:31:28 -0400

    function_auto_path ..\portfolio_qa\public\src\portfolio_qa_login_locked_test.mjs

--- public/src/portfolio_qa_login_locked_test.mjs
+++ public/src/portfolio_qa_login_locked_test.mjs
@@ -1,13 +1,11 @@
 import { portfolio_qa_login_click } from "../../../portfolio_qa/public/src/portfolio_qa_login_click.mjs";
 import { portfolio_qa_password_type } from "../../../portfolio_qa/public/src/portfolio_qa_password_type.mjs";
 import { portfolio_qa_username_valid } from "../../../portfolio_qa/public/src/portfolio_qa_username_valid.mjs";
-import { portfolio_qa_exists_assert } from "../../../portfolio_qa/public/src/portfolio_qa_exists_assert.mjs";
 import { portfolio_qa_password_valid } from "../../../portfolio_qa/public/src/portfolio_qa_password_valid.mjs";
 import { portfolio_qa_test_generic } from "../../../portfolio_qa/public/src/portfolio_qa_test_generic.mjs";
 export async function portfolio_qa_login_locked_test() {
   async function lambda(page) {
     const password = portfolio_qa_password_valid();
-    await portfolio_qa_exists_assert(page, "login-container");
     await portfolio_qa_username_valid(page);
     await portfolio_qa_password_type(page, password);
     await portfolio_qa_login_click(page);
