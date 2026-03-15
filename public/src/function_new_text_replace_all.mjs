cf410ea5710682a4301e95f6b3f8759435a1ccd8
Grace-Jared <christrosetolife@gmail.com>
2026-03-15 14:39:23 -0400

    function_auto_path ..\portfolio_qa\public\src\function_new_text_replace_all.mjs

--- public/src/function_new_text_replace_all.mjs
+++ public/src/function_new_text_replace_all.mjs
@@ -1,5 +1,8 @@
+import { js_flo_body_add_return } from "../../../love/public/src/js_flo_body_add_return.mjs";
 import { function_new_transform } from "../../../love/public/src/function_new_transform.mjs";
 export async function function_new_text_replace_all(text, f_name) {
-  async function lambda(ast) {}
+  async function lambda(ast) {
+    let r = js_flo_body_add_return(ast2);
+  }
   let output = await function_new_transform(f_name, lambda);
 }
