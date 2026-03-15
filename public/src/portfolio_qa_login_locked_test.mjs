55a3ab24140a275247a8e19736c6c8f270164566
Grace-Jared <christrosetolife@gmail.com>
2026-03-15 14:31:12 -0400

    function_auto_path ..\portfolio_qa\public\src\portfolio_qa_login_locked_test.mjs

--- public/src/portfolio_qa_login_locked_test.mjs
+++ public/src/portfolio_qa_login_locked_test.mjs
@@ -1,5 +1,10 @@
+import { portfolio_qa_username_valid_password_login } from "../../../portfolio_qa/public/src/portfolio_qa_username_valid_password_login.mjs";
+import { portfolio_qa_password_valid } from "../../../portfolio_qa/public/src/portfolio_qa_password_valid.mjs";
 import { portfolio_qa_test_generic } from "../../../portfolio_qa/public/src/portfolio_qa_test_generic.mjs";
 export async function portfolio_qa_login_locked_test() {
-  async function lambda(page) {}
+  async function lambda(page) {
+    const password = portfolio_qa_password_valid();
+    await portfolio_qa_username_valid_password_login(page, password);
+  }
   await portfolio_qa_test_generic(lambda);
 }
