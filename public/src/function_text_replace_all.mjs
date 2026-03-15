e3917530c6edab76f04801d531f0c7a886c8ba6c
Grace-Jared <christrosetolife@gmail.com>
2026-03-15 15:19:12 -0400

    function_auto_path ..\portfolio_qa\public\src\function_text_replace_all.mjs

--- public/src/function_text_replace_all.mjs
+++ public/src/function_text_replace_all.mjs
@@ -1,6 +1,8 @@
+import { function_is } from "../../../love/public/src/function_is.mjs";
 import { function_text_replace_all_lambda } from "../../../portfolio_qa/public/src/function_text_replace_all_lambda.mjs";
 import { functions_transform } from "../../../love/public/src/functions_transform.mjs";
 export async function function_text_replace_all(f_name, text) {
+  let fi2 = function_is(f);
   async function lambda2(ast) {
     function_text_replace_all_lambda(ast, f_name, text);
   }
