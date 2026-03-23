71d8947a28b91e4d858866648c95afb7e661201f
Grace-Jared <christrosetolife@gmail.com>
2026-03-22 22:30:04 -0400

    function_auto_path ..\portfolio_qa\public\src\portfolio_qa_tests_run.mjs

--- public/src/portfolio_qa_tests_run.mjs
+++ public/src/portfolio_qa_tests_run.mjs
@@ -1,28 +1,10 @@
-import { portfolio_qa_tests_run_result } from "../../../portfolio_qa/public/src/portfolio_qa_tests_run_result.mjs";
-import { catch_call_async } from "../../../love/public/src/catch_call_async.mjs";
-import { error } from "../../../love/public/src/error.mjs";
-import { list_add } from "../../../love/public/src/list_add.mjs";
-import { list_map } from "../../../love/public/src/list_map.mjs";
-import { invoke_multiple_unordered_async } from "../../../love/public/src/invoke_multiple_unordered_async.mjs";
+import { qa_tests_run } from "../../../love/public/src/qa_tests_run.mjs";
 import { function_import } from "../../../love/public/src/function_import.mjs";
 import { portfolio_qa_tests } from "../../../portfolio_qa/public/src/portfolio_qa_tests.mjs";
 import { list_map_unordered_async } from "../../../love/public/src/list_map_unordered_async.mjs";
 export async function portfolio_qa_tests_run() {
   let r = await portfolio_qa_tests();
   let mapped = await list_map_unordered_async(r, function_import);
-  let errors = [];
-  function lambda(item) {
-    function on_error(error) {
-      list_add(errors, {
-        name: item.name,
-        error,
-      });
-    }
-    let i = catch_call_async(on_error, item);
-    return i;
-  }
-  let mapped2 = list_map(mapped, lambda);
-  let r2 = await invoke_multiple_unordered_async(mapped2);
-  let result = portfolio_qa_tests_run_result(r, errors);
+  let result = await qa_tests_run(mapped, r);
   return result;
 }
