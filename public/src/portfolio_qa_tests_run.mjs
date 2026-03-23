8ed2d5f248436c8b9b1d9a2a01fc12b5d6fbeb9f
Grace-Jared <christrosetolife@gmail.com>
2026-03-22 22:34:33 -0400

    function_auto_path ..\portfolio_qa\public\src\portfolio_qa_tests_run.mjs

--- public/src/portfolio_qa_tests_run.mjs
+++ public/src/portfolio_qa_tests_run.mjs
@@ -1,3 +1,4 @@
+import { property_get_curried_right } from "../../../love/public/src/property_get_curried_right.mjs";
 import { qa_tests_run } from "../../../love/public/src/qa_tests_run.mjs";
 import { function_import } from "../../../love/public/src/function_import.mjs";
 import { portfolio_qa_tests } from "../../../portfolio_qa/public/src/portfolio_qa_tests.mjs";
@@ -5,6 +6,7 @@ import { list_map_unordered_async } from "../../../love/public/src/list_map_unor
 export async function portfolio_qa_tests_run() {
   let r = await portfolio_qa_tests();
   let mapped = await list_map_unordered_async(r, function_import);
+  let r2 = property_get_curried_right(property_name);
   let result = await qa_tests_run(mapped);
   return result;
 }
