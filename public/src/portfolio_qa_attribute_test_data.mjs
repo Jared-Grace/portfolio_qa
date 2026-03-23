6124fc9f6cdcc6b37edda7319decefb688146363
Grace-Jared <christrosetolife@gmail.com>
2026-03-22 23:05:19 -0400

    function_auto_path ..\portfolio_qa\public\src\portfolio_qa_attribute_test_data.mjs

--- public/src/portfolio_qa_attribute_test_data.mjs
+++ public/src/portfolio_qa_attribute_test_data.mjs
@@ -1,4 +1,5 @@
+import { qa_attribute_test_data } from "../../../love/public/src/qa_attribute_test_data.mjs";
 export function portfolio_qa_attribute_test_data() {
-  let r = "data-test";
+  let r = qa_attribute_test_data();
   return r;
 }
