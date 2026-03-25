63a4a391fcdcecead7e746d9e870968d59778d55
Grace-Jared <christrosetolife@gmail.com>
2026-03-25 19:39:34 -0400

    function_auto_path ..\portfolio_qa\public\src\playwright_by_attribute_exists.mjs

--- public/src/playwright_by_attribute_exists.mjs
+++ public/src/playwright_by_attribute_exists.mjs
@@ -1,7 +1,11 @@
+import { log } from "../../../love/public/src/log.mjs";
 import { greater_than_or_equal } from "../../../love/public/src/greater_than_or_equal.mjs";
 import { playwright_by_attribute_count } from "../../../portfolio_qa/public/src/playwright_by_attribute_count.mjs";
 export async function playwright_by_attribute_exists(page, name, value) {
   let c = await playwright_by_attribute_count(page, name, value);
+  log(playwright_by_attribute_exists.name, {
+    playwright_by_attribute_count,
+  });
   let e = greater_than_or_equal(c, 1);
   return e;
 }
