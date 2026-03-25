26113c58c0843f56fcd680b05c1c1c0d74afb8cd
Grace-Jared <christrosetolife@gmail.com>
2026-03-25 19:42:14 -0400

    function_auto_path ..\portfolio_qa\public\src\playwright_by_attribute_count.mjs

--- public/src/playwright_by_attribute_count.mjs
+++ public/src/playwright_by_attribute_count.mjs
@@ -1,5 +1,7 @@
+import { list_size } from "../../../love/public/src/list_size.mjs";
 import { playwright_by_attribute } from "../../../love/public/src/playwright_by_attribute.mjs";
 export async function playwright_by_attribute_count(page, name, value) {
+  let size = list_size(list);
   let r = await (await playwright_by_attribute(page, name, value)).count();
   return r;
 }
