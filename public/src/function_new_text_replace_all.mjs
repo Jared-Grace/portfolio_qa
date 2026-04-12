2675d776a3b8db2c784386a5b64501e370773897
JESUS rose to life <username@users.noreply.github.com>
2026-04-12 14:50:46 -0400

    function_auto_path ../love/public/src/function_new_open_transform.mjs

--- public/src/function_new_text_replace_all.mjs
+++ public/src/function_new_text_replace_all.mjs
@@ -1,12 +1,12 @@
 import { function_text_replace_all } from "../../../portfolio_qa/public/src/function_text_replace_all.mjs";
 import { js_flo_body_add_return_argument } from "../../../portfolio_qa/public/src/js_flo_body_add_return_argument.mjs";
 import { js_expression_string } from "../../../love/public/src/js_expression_string.mjs";
-import { function_new_transform_open } from "../../../love/public/src/function_new_transform_open.mjs";
+import { function_new_open_transform } from "../../../love/public/src/function_new_open_transform.mjs";
 export async function function_new_text_replace_all(text, f_name) {
   async function lambda(ast) {
     let e = js_expression_string(text);
     js_flo_body_add_return_argument(ast, e);
   }
-  let output = await function_new_transform_open(f_name, lambda);
+  let output = await function_new_open_transform(f_name, lambda);
   await function_text_replace_all(f_name);
 }
