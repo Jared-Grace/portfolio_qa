052985c349b5c1f49e6cdf5d8b28127546bb099f
JESUS rose to life <username@users.noreply.github.com>
2026-04-16 17:51:22 -0400

    function_auto_path ../love/public/src/at_least.mjs

--- public/src/playwright_by_attribute_exists.mjs
+++ public/src/playwright_by_attribute_exists.mjs
@@ -1,7 +1,7 @@
-import { greater_than_or_equal } from "../../../love/public/src/greater_than_or_equal.mjs";
+import { at_least } from "../../../love/public/src/at_least.mjs";
 import { playwright_by_attribute_count } from "../../../portfolio_qa/public/src/playwright_by_attribute_count.mjs";
 export async function playwright_by_attribute_exists(page, name, value) {
   let c = await playwright_by_attribute_count(page, name, value);
-  let e = greater_than_or_equal(c, 1);
+  let e = at_least(c, 1);
   return e;
 }
