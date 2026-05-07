17a9aaba6a6dc7616a730c6f5b58cb9b9d56bdf5
JESUS rose to life <username@users.noreply.github.com>
2026-05-07 18:00:04 -0400

    function_auto_path ../portfolio_qa/public/src/playwright_by_attribute_text.mjs

--- public/src/playwright_by_attribute_text.mjs
+++ public/src/playwright_by_attribute_text.mjs
@@ -1,6 +1,7 @@
+import { playwright_text_content } from "../../../love/public/src/playwright_text_content.mjs";
 import { playwright_by_attribute } from "../../../love/public/src/playwright_by_attribute.mjs";
 export async function playwright_by_attribute_text(page, name, value) {
   const e = await playwright_by_attribute(page, name, value);
-  let actual = await e.textContent();
+  let actual = await playwright_text_content(e);
   return actual;
 }
