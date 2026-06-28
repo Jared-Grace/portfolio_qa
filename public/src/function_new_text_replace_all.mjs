7991b6393f7ed806fbc9167d0670d813bb024445
JESUS rose to life <username@users.noreply.github.com>
2026-06-27 22:36:08 -0400

    repo_functions_move_if_includes_not l sat

--- public/src/function_new_text_replace_all.mjs
+++ public/src/function_new_text_replace_all.mjs
@@ -1,4 +1,4 @@
-import { function_new_text } from "../../../p_np/public/src/function_new_text.mjs";
+import { function_new_text } from "../../../love/public/src/function_new_text.mjs";
 import { function_text_replace_all } from "../../../portfolio_qa/public/src/function_text_replace_all.mjs";
 export async function function_new_text_replace_all(text, f_name) {
   await function_new_text(text, f_name);
