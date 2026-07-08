be2bc70b85f018730c1e5351a9ba2db98dc657f9
JESUS rose to life <username@users.noreply.github.com>
2026-07-08 01:02:26 -0400

    pb function_new_text

--- public/src/function_new_text_replace_all.mjs
+++ public/src/function_new_text_replace_all.mjs
@@ -1,6 +1,6 @@
 import { function_new_text } from "../../../love/public/src/function_new_text.mjs";
 import { function_text_replace_all } from "../../../portfolio_qa/public/src/function_text_replace_all.mjs";
 export async function function_new_text_replace_all(text, f_name) {
-  await function_new_text(text, f_name);
+  await function_new_text(f_name, text);
   await function_text_replace_all(f_name);
 }
