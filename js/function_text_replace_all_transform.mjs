269b2da6040cb6afe4281e3287293121dd9374ee
JESUS rose to life <username@users.noreply.github.com>
2026-07-11 23:27:22 -0400

    ai

--- js/function_text_replace_all_transform.mjs
+++ js/function_text_replace_all_transform.mjs
@@ -1,4 +1,4 @@
-import { function_text_replace_all_lambda_curried_right_ } from "../../../portfolio_qa/public/src/function_text_replace_all_lambda_curried_right_2.mjs";
+import { function_text_replace_all_lambda_curried_right_ } from "./function_text_replace_all_lambda_curried_right_2.mjs";
 import { function_run } from "../../love/js/function_run.mjs";
 export async function function_text_replace_all_transform(f_name) {
   let text = await function_run(f_name, []);
