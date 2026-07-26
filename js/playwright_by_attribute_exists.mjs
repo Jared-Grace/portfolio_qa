8a4df5de0474e1103bf36441cabf1f99292effbb
JESUS rose to life <username@users.noreply.github.com>
2026-07-26 19:08:42 +0800

    ai

--- js/playwright_by_attribute_exists.mjs
+++ js/playwright_by_attribute_exists.mjs
@@ -1,7 +1,7 @@
-import { at_least } from "../../love/js/at_least.mjs";
+import { greater_than_equal } from "../../love/js/greater_than_equal.mjs";
 import { playwright_by_attribute_count } from "./playwright_by_attribute_count.mjs";
 export async function playwright_by_attribute_exists(page, name, value) {
   let c = await playwright_by_attribute_count(page, name, value);
-  let e = at_least(c, 1);
+  let e = greater_than_equal(c, 1);
   return e;
 }
